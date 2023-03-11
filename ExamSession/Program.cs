44f8603912f6541354b944534cb9a3196f28bac9
YtkaYtkaGagaga <sersemyon@gmail.com>
2023-03-11 10:55:55 +0300

    Добавьте файлы проекта.

---
+++ ExamSession/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+namespace ExamSession
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;";
+
+            // Создание подключения
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                // Открываем подключение
+                await connection.OpenAsync();
+                Console.WriteLine("Подключение открыто");
+                // Вывод информации о подключении
+                Console.WriteLine("Свойства подключения:");
+                Console.WriteLine($"\tСтрока подключения: {connection.ConnectionString}");
+                Console.WriteLine($"\tБаза данных: {connection.Database}");
+                Console.WriteLine($"\tСервер: {connection.DataSource}");
+                Console.WriteLine($"\tВерсия сервера: {connection.ServerVersion}");
+                Console.WriteLine($"\tСостояние: {connection.State}");
+                Console.WriteLine($"\tWorkstationld: {connection.WorkstationId}");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // если подключение открыто
+                if (connection.State == ConnectionState.Open)
+                {
+                    // закрываем подключение
+                    await connection.CloseAsync();
+                    Console.WriteLine("Подключение закрыто...");
+                }
+            }
+            Console.WriteLine("Программа завершила работу.");
+            Console.Read();
+        }
+    }
+}
